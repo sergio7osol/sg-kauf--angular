725cd5ffe46fe0f9a7776fbe2829010a11915749
Sergey Osokin <sergey.osokin@netfire.eu>
2022-03-12 17:25:20 +0100

    Set up project with global settings and styles. Made basic setup for app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from "./main-header/main-header.component";
+import { LeftMenuComponent } from './left-menu/left-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    LeftMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
