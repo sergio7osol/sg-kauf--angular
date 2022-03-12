aca5fa54c1e07f8df52ea94e599f7dc996fe311e
Sergey Osokin <sergey.osokin@netfire.eu>
2022-03-12 16:56:46 +0100

    Created initial main header component.

---
+++ src/app/main-header/main-header.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header',
+  templateUrl: './main-header.component.html',
+  styleUrls: ['./main-header.component.scss']
+})
+export class MainHeaderComponent {
+  @Input() title: string = 'Project Name';
+}
